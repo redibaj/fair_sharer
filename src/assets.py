4352549310f38c3631505e4f062eb2dca72f8c19
redibaj <149614500+redibaj@users.noreply.github.com>
2023-12-06 09:02:38 +0100

    Update assets.py

--- src/assets.py
+++ src/assets.py
@@ -1,16 +1,25 @@
 def fair_sharer(values, num_iterations, share=0.1):
-"""Runs num_iterations.
-In each iteration the highest value in values gives a fraction (share)
-to both the left and right neighbor. The leftmost field is considered
-the neightbor of the rightmost field.
-Examples:
-fair_sharer([0, 1000, 800, 0], 1) --> [100, 800, 900, 0]
-fair_sharer([0, 1000, 800, 0], 2) --> [100, 890, 720, 90]
-Args
-values:
-1D array of values (list or numpy array)
-num_iteration:
-Integer to set the number of iterations
-"""
-# code
-return values_new
+    """
+    Runs num_iterations.
+    In each iteration the highest value in values gives a fraction (share)
+    to both the left and right neighbor. The leftmost field is considered
+    the neighbor of the rightmost field.
+    """
+    for _ in range(num_iterations):
+        max_index = values.index(max(values))
+
+        share_value = values[max_index] * share
+
+        left_neighbor = (max_index - 1) % len(values)
+        right_neighbor = (max_index + 1) % len(values)
+
+        values[left_neighbor] += share_value
+        values[right_neighbor] += share_value
+        values[max_index] -= 2 * share_value
+
+    return values
+
+fair_sharer([0, 1000, 800, 0], 1)
+fair_sharer([0, 1000, 800, 0], 2)
+
+fair_sharer 
